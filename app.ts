20fa6808629178edcf4787dbc7f4538eba8c93da
ashrafomid2025 <omidmashraf@gmail.com>
2025-06-16 21:59:21 +0430

    data type in ts

--- app.ts
+++ app.ts
@@ -1,2 +1,12 @@
-const message: string = "Hi typescript course";
-console.log(message);
+// const message: string = "Hi typescript course";
+// console.log(message);
+
+let myName: string = "Mohammad Ashraf Omid";
+let age: number = 29;
+let isProgrammer = true;
+
+let city: string = "Ghazni";
+let numbers: number[] = [1, 2, 3, 4, 5, 7];
+let cities: Array<string> = ["Kabul", "Ghazni", "Herat"];
+
+let person: [string, number] = ["Ali", 30];
